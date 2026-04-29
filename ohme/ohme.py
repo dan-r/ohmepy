907d914d9414d09ecda0c378766104ec31db6411
Daniel Raper <me@danr.uk>
2026-04-29 22:06:13 +0100

    Move data models to separate file

--- ohme/ohme.py
+++ ohme/ohme.py
@@ -12,101 +12,15 @@ import datetime
 import aiohttp
 from .utils import ChargeSlot, slot_list, vehicle_to_name
 from .const import VERSION, GOOGLE_API_KEY
+from .models import (
+    ChargerStatus,
+    ChargerMode,
+    SummaryGranularity,
+    ChargeSummary,
+    ChargerPower,
+)
 
 _LOGGER = logging.getLogger(__name__)
-
-
-class ChargerStatus(Enum):
-    """Charger state enum."""
-
-    UNPLUGGED = "unplugged"
-    PENDING_APPROVAL = "pending_approval"
-    CHARGING = "charging"
-    PLUGGED_IN = "plugged_in"
-    PAUSED = "paused"
-    FINISHED = "finished"
-
-
-class ChargerMode(Enum):
-    """Charger mode enum."""
-
-    SMART_CHARGE = "smart_charge"
-    MAX_CHARGE = "max_charge"
-    PAUSED = "paused"
-
-
-class SummaryGranularity(Enum):
-    """Granularity for charge summary data."""
-
-    DAY = "DAY"
-    HOUR = "HOUR"
-
-
-class Money(TypedDict):
-    currencyCode: str
-    amount: str
-
-
-class ChargeStat(TypedDict):
-    ownerUserId: str
-    deviceId: Optional[str]
-    energyChargedTotalWh: int
-    solarEnergyChargedWh: int
-    startTime: int
-    endTime: int
-    activeChargeMs: int
-    location: Optional[str]
-    locationType: Optional[str]
-
-    carbonStats: TypedDict(
-        "CarbonStats",
-        {
-            "carbonReleasedGreenScore": float,
-            "carbonReleasedPerKmGrams": int,
-            "carbonReleasedGrams": int,
-            "carbonReleasedRegularCableGrams": int,
-            "carbonSavedVsRegularCableGrams": int,
-            "carbonReleasedGasCarGrams": int,
-            "carbonSavedVsGasCarGrams": int,
-            "comparedGasCarLabel": Optional[str],
-        },
-        total=False,
-    )
-
-    costStats: TypedDict(
-        "CostStats",
-        {
-            "moneyCostTotal": Money,
-            "moneyCostStandardTariff": Money,
-            "moneySavedVsStandardTariff": Money,
-            "moneyCostPerKm": Money,
-            "averageKwhPrice": Money,
-        },
-    )
-
-    batteryStats: TypedDict(
-        "BatteryStats",
-        {
-            "batteryScore": float,
-            "batteryCycleUsePercent": int,
-            "rangeAddedKm": float,
-        },
-    )
-
-
-class ChargeSummary(TypedDict):
-    totalStats: ChargeStat
-    stats: List[ChargeStat]
-    granularity: SummaryGranularity
-
-
-@dataclass
-class ChargerPower:
-    """Dataclass for reporting power status of charger."""
-
-    watts: float
-    amps: float
-    volts: int | None
 
 
 class OhmeApiClient:
